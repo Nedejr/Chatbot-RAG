10bad3162a7ba938aad67b2de84c05dc71362b7d
NedeJr <nedejr@gmail.com>
2025-01-28 15:52:35 -0300

    fix

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 
 
 load_dotenv()
